b833e90702ee35754f1e77d821e1d878ef06540c
i5ting <i5ting@126.com>
2013-04-11 13:39:00 +0800

    refact first_init method name and remove warning with type

--- PageListViewController.h
+++ PageListViewController.h
@@ -17,11 +17,37 @@
 
 -(void)init_table_data;
 -(void)reload_next_page:(int)cur_page_number;
+
+- (UITableViewCell *)page_list_view:(UITableView *)tableView cell_for_row_at_index_path:(NSIndexPath *)indexPath;
+- (void)page_list_view:(UITableView *)tableView did_select_row_at_index_path:(NSIndexPath *)indexPath;
+- (CGFloat)page_list_view:(UITableView *)tableView height_for_row_at_index_path:(NSIndexPath *)indexPath;
+//@optional
+//- (UITableViewCell *)page_list_view_next_page:(UITableView *)tableView cell_for_row_at_index_path:(NSIndexPath *)indexPath;
+//- (void)page_list_view_next_page:(UITableView *)tableView did_select_row_at_index_path:(NSIndexPath *)indexPath;
+//- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath;
 @end
 
-@interface PageListViewController : ListViewController <UITableViewDataSource, UITableViewDelegate>
+@protocol PageListViewWithModeControllerProtocol <PageListViewControllerProtocol>
 
-@property(nonatomic,assign,readwrite) id <PageListViewControllerProtocol>delegate;
+@required
+- (UITableViewCell *)page_list_view_next_page:(UITableView *)tableView cell_for_row_at_index_path:(NSIndexPath *)indexPath;
+- (void)page_list_view_next_page:(UITableView *)tableView did_select_row_at_index_path:(NSIndexPath *)indexPath;
+- (CGFloat)page_list_view_next_page:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath;
+
+
+@optional
+//如果没有实现page_list_view_next_page方法，则同所有cell高度
+- (CGFloat)page_list_view_next_page:(UITableView *)tableView height_for_row_at_index_path:(NSIndexPath *)indexPath;
+@end
+
+
+
+@interface PageListViewController : ListViewController <UITableViewDataSource, UITableViewDelegate>
+{
+    Boolean _has_more_page;
+}
+
+@property(nonatomic,assign,readwrite) id <PageListViewWithModeControllerProtocol>delegate;
  
 
 @property(nonatomic,retain,readwrite) NSMutableArray *result_array;
@@ -32,4 +58,25 @@
 
 -(void)init_table_data_callback:(NSArray *)r;
 -(void)reload_next_page_callback:(NSArray *)r;
+
+-(void)get_next_page;
+
+@end
+
+
+
+
+typedef enum {
+    PageListViewModeNone = 0,
+    PageListViewModeDrag = 1,
+    PageListViewModeCell = 2
+} PageListViewMode;
+
+
+@interface PageListViewWithModeController : PageListViewController{
+    PageListViewMode _mode;
+}
+
+-(id)init_with_frame:(CGRect)frame  mode:(PageListViewMode)mode;
+
 @end
--- PageListViewController.m
+++ PageListViewController.m
@@ -8,6 +8,14 @@
 
 #import "PageListViewController.h"
 
+@interface PageListViewController()
+
+//根据返回数据长度，判断是否有下一页
+-(void)set_is_has_next_page:(int)cur_arrry_count;
+
+
+@end
+
 @implementation PageListViewController
 @synthesize delegate;
 @synthesize result_array,cur_page_number,page_count;
@@ -25,7 +33,6 @@
     
     self.cur_page_number = 1;
     // Here you would make an HTTP request or something like that
-    
     
     if ([delegate respondsToSelector:@selector(init_table_data)]) {
          [delegate init_table_data];
@@ -46,12 +53,12 @@
     
     NSLog(@"-----%@",[NSString stringWithFormat:@"%d",self.cur_page_number]);
     
+    [self set_is_has_next_page:[r count]];
     [self.table reloadData];
     
     // Call [self doneLoadingTableViewData] when you are done
     [self performSelector:@selector(doneLoadingTableViewData) withObject:nil afterDelay:1.0];
 }
-
 
 
 
@@ -71,7 +78,8 @@
     NSMutableArray *r = [NSMutableArray arrayWithArray:self.result_array];
     [r addObjectsFromArray:p];
     self.result_array = r;
-    
+      
+    [self set_is_has_next_page:[p count]];
     [self.table reloadData];
 }
 
@@ -86,25 +94,25 @@
     return  [self.result_array count];
 }
 
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    
-    static NSString *CellIdentifier = @"Cell";
-    
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-    if (cell == nil) {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] ;
-    }
-    
-    NSDictionary *d = [self.result_array objectAtIndex:indexPath.row];
-    if ([[d objectForKey:@"news"] count]>0) {
-        NSString *title = [[[d objectForKey:@"news"] objectAtIndex:0] objectForKey:@"title"];
-        cell.textLabel.text = [NSString stringWithFormat:@"#%d %@", indexPath.row,title];
-    }else{
-        cell.textLabel.text = [NSString stringWithFormat:@"#%d  ", indexPath.row];
-    }
-    
-    return cell;
-}
+//- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+//    
+//    static NSString *CellIdentifier = @"Cell";
+//    
+//    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+//    if (cell == nil) {
+//        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] ;
+//    }
+//    
+//    NSDictionary *d = [self.result_array objectAtIndex:indexPath.row];
+//    if ([[d objectForKey:@"news"] count]>0) {
+//        NSString *title = [[[d objectForKey:@"news"] objectAtIndex:0] objectForKey:@"title"];
+//        cell.textLabel.text = [NSString stringWithFormat:@"#%d %@", indexPath.row,title];
+//    }else{
+//        cell.textLabel.text = [NSString stringWithFormat:@"#%d  ", indexPath.row];
+//    }
+//    
+//    return cell;
+//}
 
 //- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
 //	return [NSString stringWithFormat:@"Section %i", section];
@@ -117,34 +125,28 @@
     NSLog(@"selected cell at section #%d and row #%d", indexPath.section, indexPath.row);
 }
 
-#pragma mark - View lifecycle
-
+#pragma mark - UIScrollViewDelegate
 
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView{
-    
-    int contentHeight = scrollView.contentSize.height;
-    contentHeight = contentHeight>=scrollView.bounds.size.height?contentHeight:scrollView.bounds.size.height;
-    int extraHeight = contentHeight - scrollView.bounds.size.height;
-    if (scrollView.contentOffset.y>extraHeight + 45) {
-        
-        [NSObject cancelPreviousPerformRequestsWithTarget:self selector:@selector(reloadNextPage) object:nil];
-        [self performSelector:@selector(reloadNextPage) withObject:nil afterDelay:.1];
+    if (_has_more_page) {
+        int contentHeight = scrollView.contentSize.height;
+        contentHeight = contentHeight>=scrollView.bounds.size.height?contentHeight:scrollView.bounds.size.height;
+        int extraHeight = contentHeight - scrollView.bounds.size.height;
+        if (scrollView.contentOffset.y>extraHeight + 45) {
+            [self get_next_page];
+        }
     }
 }
 
-
+#pragma mark - View lifecycle
 
 - (void)viewWillAppear:(BOOL)animated{
     [self reloadTableViewDataSource];
 }
 
-
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.title = @"Pull down example";
-   
 }
 
 - (void)viewDidUnload
@@ -157,4 +159,183 @@
     return YES;
 }
 
+
+#pragma mark - Public methods
+
+-(void)get_next_page{
+    [NSObject cancelPreviousPerformRequestsWithTarget:self selector:@selector(reloadNextPage) object:nil];
+    [self performSelector:@selector(reloadNextPage) withObject:nil afterDelay:.1];
+}
+
+#pragma mark - Private methods
+
+-(void)set_is_has_next_page:(int)cur_arrry_count
+{
+    //根据返回数据长度，判断是否有下一页
+    if (cur_arrry_count == self.page_count) {
+        _has_more_page = YES;
+    }else{
+        _has_more_page = NO;
+    }
+}
+
 @end
+
+
+
+
+
+
+@implementation PageListViewWithModeController
+
+
+-(id)init_with_frame:(CGRect)frame  mode:(PageListViewMode)mode{
+    if (self = [super initWithFrame:frame]) {
+        _mode = mode;
+    }
+    return self;
+}
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    switch (_mode) {
+        case PageListViewModeNone:
+             return  [self.result_array count];
+            break;
+        case PageListViewModeDrag:
+            return  [self.result_array count];
+            break;
+            
+        case PageListViewModeCell:
+            return  [self.result_array count] + 1;
+            break;
+            
+        default:
+            break;
+    }
+    
+    return  0;
+}
+
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView{
+    if (_mode == PageListViewModeCell || _mode == PageListViewModeDrag) {
+        if (_has_more_page) {
+            int contentHeight = scrollView.contentSize.height;
+            contentHeight = contentHeight>=scrollView.bounds.size.height?contentHeight:scrollView.bounds.size.height;
+            int extraHeight = contentHeight - scrollView.bounds.size.height;
+            if (scrollView.contentOffset.y>extraHeight + 45) {
+                [NSObject cancelPreviousPerformRequestsWithTarget:self selector:@selector(reloadNextPage) object:nil];
+                [self performSelector:@selector(reloadNextPage) withObject:nil afterDelay:.1];
+            }
+        }
+    }
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (![self.delegate respondsToSelector:@selector(page_list_view_next_page:cell_for_row_at_index_path:)]) {
+        assert(@"cellForRowAtIndexPath fail");
+    }
+    
+    switch (_mode) {
+        case PageListViewModeNone:
+            return  [self.delegate page_list_view:tableView cell_for_row_at_index_path:indexPath];
+            break;
+        case PageListViewModeDrag:
+            return  [self.delegate page_list_view:tableView cell_for_row_at_index_path:indexPath];
+            break;
+        case PageListViewModeCell:
+            if (indexPath.row< [self.result_array count]) {
+                return  [self.delegate page_list_view:tableView cell_for_row_at_index_path:indexPath];
+            }else{
+                return  [self.delegate page_list_view_next_page:tableView cell_for_row_at_index_path:indexPath];
+            }
+            
+            break;
+            
+        default:
+            break;
+    }
+}
+
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    if (![self.delegate respondsToSelector:@selector(page_list_view:did_select_row_at_index_path:)]) {
+        return ;
+    }
+    
+    switch (_mode) {
+        case PageListViewModeNone:
+            [self.delegate page_list_view:tableView did_select_row_at_index_path:indexPath];
+            break;
+        case PageListViewModeDrag:
+            [self.delegate page_list_view:tableView did_select_row_at_index_path:indexPath];
+            break;
+        case PageListViewModeCell:
+            if ([self.delegate respondsToSelector:@selector(page_list_view_next_page:heightForRowAtIndexPath:)]) {
+                if (indexPath.row< [self.result_array count]) {
+                    [self.delegate page_list_view:tableView did_select_row_at_index_path:indexPath];
+                }else{
+                    [self.delegate page_list_view_next_page:tableView did_select_row_at_index_path:indexPath];
+                }
+            }else{
+                [self page_list_view_next_page:tableView did_select_row_at_index_path:indexPath];
+            }
+            break;
+            
+        default:
+            break;
+    }
+}
+
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (![self.delegate respondsToSelector:@selector(page_list_view_next_page:height_for_row_at_index_path:)]) {
+        return 44.0f;
+    }
+    switch (_mode) {
+        case PageListViewModeNone:
+            return [self.delegate page_list_view:tableView height_for_row_at_index_path:indexPath];
+            break;
+        case PageListViewModeDrag:
+            return [self.delegate page_list_view:tableView height_for_row_at_index_path:indexPath];
+            break;
+        case PageListViewModeCell:
+            //如果没有实现page_list_view_next_page方法，则同所有cell高度
+            if ([self.delegate respondsToSelector:@selector(page_list_view_next_page:height_for_row_at_index_path:)]) {
+                if (indexPath.row< [self.result_array count]) {
+                    return [self.delegate page_list_view:tableView height_for_row_at_index_path:indexPath];
+                }else{
+                    return [self.delegate page_list_view_next_page:tableView height_for_row_at_index_path:indexPath];
+                }
+            }else{
+                return [self.delegate page_list_view:tableView height_for_row_at_index_path:indexPath];
+            }
+            
+            break;
+            
+        default:
+            break;
+    }
+
+}
+
+
+- (void)page_list_view_next_page:(UITableView *)tableView did_select_row_at_index_path:(NSIndexPath *)indexPath
+{
+    NSLog(@"sss");
+    
+    [self get_next_page];
+}
+
+@end
+
+
+
+
+
+
+
